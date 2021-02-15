96db343c8c00d0c4d20ef57dc41d4e166f99d5b7
sj <sebastianthegreatful@gmail.com>
2021-02-15 11:48:03 +0700

    Increase sleep time

--- rebuild-cluster.sh
+++ rebuild-cluster.sh
@@ -74,7 +74,7 @@ ip=""
 while [ -z "$ip" ]; do
   echo "Waiting for external IP"
   ip=$(kubectl get --namespace cloud-endpoints svc/esp-echo --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
-  [ -z "$ip" ] && sleep 1
+  [ -z "$ip" ] && sleep 3
 done
 echo "IP: $ip"
 
